510a420c71d90fa99b146d9f0e3d5e7b7d1089eb
Long.cntt.bkhn <hoanglong180695@gmail.com>
2016-04-23 10:55:20 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>My CMS</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script
+	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<!-- <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button> -->
+				<a class="navbar-brand" href="#">My CMS</a>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse"
+				id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li><a href="#">About me</a></li>
+					<li><a href="#">Services</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+
+				<form class="navbar-form navbar-right search-form" role="search">
+					<div class="form-group has-feedback">
+						<!-- <label for="search" class="sr-only">Search</label> -->
+						<input type="text" class="form-control" name="search" id="search"
+							placeholder="search"> <span
+							class="glyphicon glyphicon-search form-control-feedback"></span>
+					</div>
+				</form>
+				<div class="nav navbar-btn navbar-right">
+					<button type="button" class="btn btn-success">Đăng ký</button>
+					<button type="button" class="btn btn-info">Đăng nhập</button>
+				</div>
+			</div>
+		</div>
+		<!-- /.navbar-collapse -->
+
+		<!-- /.container -->
+	</nav>
+
+</body>
+</html>

138811464557d2fb7cf76c3de4b785d3c3491d32
Long.cntt.bkhn <hoanglong180695@gmail.com>
2016-04-23 15:31:06 +0700

    create master page

---
+++ resources/views/pages/newPage.blade.php
@@ -0,0 +1,74 @@
+@extends('template.master') @section('content')
+<div class="list-group">
+	<div class="media list-group-item">
+		<div class="media-left">
+			<a href="#"> <img class="media-object" src="image/a1.jpeg" alt="">
+			</a>
+		</div>
+		<div class="media-body">
+			<h4 class="media-heading">
+				<a>Tiêu đề bài viết</a>
+			</h4>
+			<h6>12/3/2015</h6>
+			<p>Nội dung tóm tắt</p>
+			<a href="#">Xem chi tiết</a>
+		</div>
+	</div>
+	<div class="media list-group-item">
+		<div class="media-left">
+			<a href="#"> <img class="media-object" src="image/a2.jpeg" alt="">
+			</a>
+		</div>
+		<div class="media-body">
+			<h4 class="media-heading">
+				<a>Tiêu đề bài viết</a>
+			</h4>
+			<h6>12/3/2015</h6>
+			<p>Nội dung tóm tắt</p>
+			<a href="#">Xem chi tiết</a>
+		</div>
+	</div>
+	<div class="media list-group-item">
+		<div class="media-left">
+			<a href="#"> <img class="media-object" src="image/a3.jpeg" alt="">
+			</a>
+		</div>
+		<div class="media-body">
+			<h4 class="media-heading">
+				<a>Tiêu đề bài viết</a>
+			</h4>
+			<h6>12/3/2015</h6>
+			<p>Nội dung tóm tắt</p>
+			<a href="#">Xem chi tiết</a>
+		</div>
+	</div>
+	<div class="media list-group-item">
+		<div class="media-left">
+			<a href="#"> <img class="media-object" src="image/a4.jpeg" alt="">
+			</a>
+		</div>
+		<div class="media-body">
+			<h4 class="media-heading">
+				<a>Tiêu đề bài viết</a>
+			</h4>
+			<h6>12/3/2015</h6>
+			<p>Nội dung tóm tắt</p>
+			<a href="#">Xem chi tiết</a>
+		</div>
+	</div>
+	<div class="media list-group-item">
+		<div class="media-left">
+			<a href="#"> <img class="media-object" src="image/a5.jpeg" alt="">
+			</a>
+		</div>
+		<div class="media-body">
+			<h4 class="media-heading">
+				<a>Tiêu đề bài viết</a>
+			</h4>
+			<h6>12/3/2015</h6>
+			<p>Nội dung tóm tắt</p>
+			<a href="#">Xem chi tiết</a>
+		</div>
+	</div>
+</div>
+@stop
--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -49,68 +49,7 @@
 					by <a href="#">team</a>
 				</p>
 				<hr>
-				<div class="list-group">
-					<div class="media list-group-item">
-						<div class="media-left">
-							<a href="#"> <img class="media-object" src="image/a1.jpeg" alt="">
-							</a>
-						</div>
-						<div class="media-body">
-							<h4 class="media-heading"><a>Tiêu đề bài viết</a></h4>
-							<h6>12/3/2015</h6>
-							<p>Nội dung tóm tắt</p>
-							<a href="#">Xem chi tiết</a>
-						</div>
-					</div>
-					<div class="media list-group-item">
-						<div class="media-left">
-							<a href="#"> <img class="media-object" src="image/a2.jpeg" alt="">
-							</a>
-						</div>
-						<div class="media-body">
-							<h4 class="media-heading"><a>Tiêu đề bài viết</a></h4>
-							<h6>12/3/2015</h6>
-							<p>Nội dung tóm tắt</p>
-							<a href="#">Xem chi tiết</a>
-						</div>
-					</div>
-					<div class="media list-group-item">
-						<div class="media-left">
-							<a href="#"> <img class="media-object" src="image/a3.jpeg" alt="">
-							</a>
-						</div>
-						<div class="media-body">
-							<h4 class="media-heading"><a>Tiêu đề bài viết</a></h4>
-							<h6>12/3/2015</h6>
-							<p>Nội dung tóm tắt</p>
-							<a href="#">Xem chi tiết</a>
-						</div>
-					</div>
-					<div class="media list-group-item">
-						<div class="media-left">
-							<a href="#"> <img class="media-object" src="image/a4.jpeg" alt="">
-							</a>
-						</div>
-						<div class="media-body">
-							<h4 class="media-heading"><a>Tiêu đề bài viết</a></h4>
-							<h6>12/3/2015</h6>
-							<p>Nội dung tóm tắt</p>
-							<a href="#">Xem chi tiết</a>
-						</div>
-					</div>
-					<div class="media list-group-item">
-						<div class="media-left">
-							<a href="#"> <img class="media-object" src="image/a5.jpeg" alt="">
-							</a>
-						</div>
-						<div class="media-body">
-							<h4 class="media-heading"><a>Tiêu đề bài viết</a></h4>
-							<h6>12/3/2015</h6>
-							<p>Nội dung tóm tắt</p>
-							<a href="#">Xem chi tiết</a>
-						</div>
-					</div>
-				</div>
+				@yield('content')	
 			</div>
 			<div class="col-md-4">
 				<div class="panel panel-default panel-body">
@@ -153,3 +92,4 @@
 	</div>
 </body>
 </html>
+
